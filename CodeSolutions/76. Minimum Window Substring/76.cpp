a02fdc5d51e5fd433fe164a992da576784a0d75a
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2017-08-19 17:16:00 +0530

    Add new solutions.

---
+++ CodeSolutions/76. Minimum Window Substring/76.cpp
@@ -0,0 +1,39 @@
+# Language: Python
+# Commit Timestamp: 2022-06-28T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2022-06-28T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2022-06-28T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+ public:
+  string minWindow(string s, string t) {
+    vector<int> count(128);
+    int required = t.length();
+    int bestLeft = -1;
+    int minLength = s.length() + 1;
+
+    for (const char c : t)
+      ++count[c];
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      if (--count[s[r]] >= 0)
+        --required;
+      while (required == 0) {
+        if (r - l + 1 < minLength) {
+          bestLeft = l;
+          minLength = r - l + 1;
+        }
+        if (++count[s[l++]] > 0)
+          ++required;
+      }
+    }
+
+    return bestLeft == -1 ? "" : s.substr(bestLeft, minLength);
+  }
+};
---
+++ CodeSolutions/76. Minimum Window Substring/76.java
@@ -0,0 +1,38 @@
+# Language: Python
+# Commit Timestamp: 2022-04-29T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2022-04-29T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2022-04-29T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+  public String minWindow(String s, String t) {
+    int[] count = new int[128];
+    int required = t.length();
+    int bestLeft = -1;
+    int minLength = s.length() + 1;
+
+    for (final char c : t.toCharArray())
+      ++count[c];
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      if (--count[s.charAt(r)] >= 0)
+        --required;
+      while (required == 0) {
+        if (r - l + 1 < minLength) {
+          bestLeft = l;
+          minLength = r - l + 1;
+        }
+        if (++count[s.charAt(l++)] > 0)
+          ++required;
+      }
+    }
+
+    return bestLeft == -1 ? "" : s.substring(bestLeft, bestLeft + minLength);
+  }
+}
