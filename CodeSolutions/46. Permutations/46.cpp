f71edec53bbc56a7dab4a781b5c5cabe4f6545da
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2018-09-02 10:27:00 +0530

    Add new solutions.

---
+++ CodeSolutions/46. Permutations/46.cpp
@@ -0,0 +1,36 @@
+# Language: Python
+# Commit Timestamp: 2025-06-12T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2025-06-12T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+ public:
+  vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+
+    dfs(nums, vector<bool>(nums.size()), {}, ans);
+    return ans;
+  }
+
+ private:
+  void dfs(const vector<int>& nums, vector<bool>&& used, vector<int>&& path,
+           vector<vector<int>>& ans) {
+    if (path.size() == nums.size()) {
+      ans.push_back(path);
+      return;
+    }
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (used[i])
+        continue;
+      used[i] = true;
+      path.push_back(nums[i]);
+      dfs(nums, std::move(used), std::move(path), ans);
+      path.pop_back();
+      used[i] = false;
+    }
+  }
+};
