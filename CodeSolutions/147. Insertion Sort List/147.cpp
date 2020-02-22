8505e930c148754e9a214be210b30d5fdedf7d50
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2020-02-22 16:50:00 +0530

    Add new solutions.

---
+++ CodeSolutions/147. Insertion Sort List/147.cpp
@@ -0,0 +1,28 @@
+# Language: Python
+# Commit Timestamp: 2026-11-04T00:00:00-04:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2026-11-04T00:00:00-04:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+ public:
+  ListNode* insertionSortList(ListNode* head) {
+    ListNode dummy(0);
+    ListNode* prev = &dummy;  // the last and thus largest of the sorted list
+
+    while (head != nullptr) {       // the current inserting node
+      ListNode* next = head->next;  // Cache the next inserting node.
+      if (prev->val >= head->val)
+        prev = &dummy;  // Move `prev` to the front.
+      while (prev->next && prev->next->val < head->val)
+        prev = prev->next;
+      head->next = prev->next;
+      prev->next = head;
+      head = next;  // Update the current inserting node.
+    }
+
+    return dummy.next;
+  }
+};
