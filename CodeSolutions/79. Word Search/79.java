75bf55a0f2751843b132b4b90792a1cba55c7b48
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2022-04-21 20:33:00 -0300

    Add new solutions.

---
+++ CodeSolutions/79. Word Search/79.java
@@ -0,0 +1,36 @@
+# Language: Python
+# Commit Timestamp: 2028-12-23T00:00:00-04:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2028-12-23T00:00:00-04:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+  public boolean exist(char[][] board, String word) {
+    for (int i = 0; i < board.length; ++i)
+      for (int j = 0; j < board[0].length; ++j)
+        if (dfs(board, word, i, j, 0))
+          return true;
+    return false;
+  }
+
+  private boolean dfs(char[][] board, String word, int i, int j, int s) {
+    if (i < 0 || i == board.length || j < 0 || j == board[0].length)
+      return false;
+    if (board[i][j] != word.charAt(s) || board[i][j] == '*')
+      return false;
+    if (s == word.length() - 1)
+      return true;
+
+    final char cache = board[i][j];
+    board[i][j] = '*';
+    final boolean isExist = dfs(board, word, i + 1, j, s + 1) || //
+                            dfs(board, word, i - 1, j, s + 1) || //
+                            dfs(board, word, i, j + 1, s + 1) || //
+                            dfs(board, word, i, j - 1, s + 1);
+    board[i][j] = cache;
+
+    return isExist;
+  }
+}
