98a285d4edc0cb1e503bf78192b18862b89a061b
Akhil Chaudhary <chaudharyakhilbit@gmail.com>
2023-11-03 10:19:00 -0300

    Add new solutions.

---
+++ CodeSolutions/36. Valid Sudoku/36.cpp
@@ -0,0 +1,28 @@
+# Language: Python
+# Commit Timestamp: 2030-07-16T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+# Language: Python
+# Commit Timestamp: 2030-07-16T00:00:00-03:00
+# Solution Logic: Placeholder for explanation.
+
+class Solution {
+ public:
+  bool isValidSudoku(vector<vector<char>>& board) {
+    unordered_set<string> seen;
+
+    for (int i = 0; i < 9; ++i)
+      for (int j = 0; j < 9; ++j) {
+        if (board[i][j] == '.')
+          continue;
+        const string c(1, board[i][j]);
+        if (!seen.insert(c + "@row" + to_string(i)).second ||
+            !seen.insert(c + "@col" + to_string(j)).second ||
+            !seen.insert(c + "@box" + to_string(i / 3) + to_string(j / 3))
+                 .second)
+          return false;
+      }
+
+    return true;
+  }
+};
